7557dbbc36bfa6c5cd1884fdf2e19e88fc762789
tobiaszarlenga <162618439+tobiaszarlenga@users.noreply.github.com>
2024-05-03 15:13:51 -0300

    termino el punto 4

---
+++ punto4.js
@@ -0,0 +1,37 @@
+class Producto {
+  constructor(nombre, precio, codigo) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.codigo = codigo;
+  }
+
+  mostrarDetalle() {
+   return document.write(
+      `<ul>
+            <li>Nombre: ${this.nombre}</li>
+            <li>Precio: ${this.precio}</li>
+            <li>Codigo: ${this.codigo}</li>
+
+
+            </ul>
+            
+            `
+    );
+  }
+}
+
+let productos = []; //array de los productos
+
+let leche = new Producto('milk', 2000, 'd43886');
+let coca = new Producto('cola', 1000, 'd4776');
+let sanguche = new Producto('chegu', 2000, 'd40036');
+
+productos.push(leche);
+
+productos.push(coca);
+
+productos.push(sanguche);
+
+for (let i= 0; i< productos.length; i++){
+productos[i].mostrarDetalle();
+}
